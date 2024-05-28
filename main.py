58ea33c1a37db5f3547cf0f83ffb65434a939a24
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2024-05-28 11:28:38 +0300

    added simple comparing tool

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import argparse
 import difflib
 
-from imx477.nvidia import (
-    imx477_mode_1920x1080_60fps,
-    imx477_mode_4032x3040_30fps
-)
-from imx477.raspberry import mode_4056x3040_regs_10fps
-from imx477.zenqberry import sensor_4k_regs_30fps_10b
-from imx477.registers import add_label
+from imx477 import nvidia, raspberry, zenqberry
+from imx577 import orangepi
+
+from imx477.registers import add_label, NAME_TO_VALUE
+
+modules = [nvidia, raspberry, zenqberry, orangepi]
 
 
 def add_register_labels(registers_list):
@@ -14,14 +14,32 @@ def add_register_labels(registers_list):
         yield f"{add_label(register)} => {str(value).rjust(10)} ({hex(value)})"
 
 
-if __name__ == "__main__":
-    low_res_mode = list(add_register_labels(sorted(imx477_mode_4032x3040_30fps)))
-    # print(low_res_mode)
+def find_mode(name: str):
+    for module in modules:
+        for (mode_name, registers) in module.modes:
+            if f'{module.__name__}.{mode_name}' == name:
+                return registers
 
-    high_res_mode = list(add_register_labels(sorted(sensor_4k_regs_30fps_10b)))
-    # print(high_res_mode)
+    raise FileNotFoundError('Unabme to find %s' % name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    available_choices = [
+        f'{module.__name__}.{mode_name}'
+        for module in modules
+        for (mode_name, _) in module.modes
+    ]
+
+    parser.add_argument("-l", "--left", choices=available_choices, required=True)
+    parser.add_argument("-r", "--right", choices=available_choices, required=True)
+
+    args = parser.parse_args()
+
+    low_res_mode = list(add_register_labels(sorted(find_mode(args.left))))
+    high_res_mode = list(add_register_labels(sorted(find_mode(args.right))))
 
     diff = difflib.ndiff(low_res_mode, high_res_mode)
     print('\n'.join(diff))
 
-    # print('\n'.join(high_res_mode))
